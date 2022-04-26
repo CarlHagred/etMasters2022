f2640b80a57b76581d13bb03b25010f086c9d43b
CarlHagred <carl.hagred@gmail.com>
2022-04-26 13:07:09 +0200

    admin endpoints setup

--- controllers/admin/adminEndpoints.js
+++ controllers/admin/adminEndpoints.js
@@ -1,7 +1,8 @@
-import Competition from '../../models/competition.js';
+import Competition from "../../models/competition.js";
+import Player from "../../models/player.js";
 
 export const postCompetition = async (req, res) => {
-  const name = 'Masters';
+  const name = "Masters";
 
   const newCompetition = new Competition({
     name: name,
@@ -9,4 +10,33 @@ export const postCompetition = async (req, res) => {
     players: [],
   });
   await newCompetition.save();
+};
+
+export const postPlayer = async (req, res) => {
+  const name = "joel"; //req.body.name;
+  const password = "trassel1"; //req.body.password;
+  const handicap = 5.9; //req.body.handicap;
+
+  Player.findOne({ name: name }, async (err, doc) => {
+    if (err) res.send(err);
+    if (!doc) {
+      const newPlayer = new Player({
+        name: name,
+        password: password,
+        handicap: handicap,
+        competition: [],
+      });
+      await newPlayer.save();
+      //res.status(201).json('Success');
+    }
+  });
+};
+
+export const getPlayers = async (req, res) => {
+  try {
+    const players = await Player.find();
+    res.status(200).json(players);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
--- controllers/players/playerEndpoints.js
+++ controllers/players/playerEndpoints.js
@@ -1,24 +1,4 @@
-import Player from '../../models/player.js';
-
-export const postPlayer = async (req, res) => {
-  const name = 'Jucke';
-  const password = 'gedenGeden';
-  const handicap = 34; // req.body.handicap;
-
-  Player.findOne({ name: name }, async (err, doc) => {
-    if (err) res.send(err);
-    if (!doc) {
-      const newPlayer = new Player({
-        name: name,
-        password: password,
-        handicap: handicap,
-        competition: [],
-      });
-      await newPlayer.save();
-      //res.status(201).json('Success');
-    }
-  });
-};
+import Player from "../../models/player.js";
 
 export const getPlayers = async (req, res) => {
   try {
