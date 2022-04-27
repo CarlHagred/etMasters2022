5bcc0f430cd43fb596c7e2e5dd96d68291515263
CarlHagred <carl.hagred@gmail.com>
2022-04-27 12:09:35 +0200

    delete round

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { dirname } from 'path';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import morgan from 'morgan';
-import routes from './routes/routes.js';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { dirname } from "path";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+import routes from "./routes/routes.js";
 import {
   postPlayer,
   deletePlayer,
   postCompetition,
   changeRoundPoint,
 } from "./controllers/admin/adminEndpoints.js";
-import { getCompetitions } from "./controllers/shared/sharedEndpoints.js";
-
-import { getRoundsPlayed, postRound } from "./controllers/players/roundEndPoint.js";
-
+import {
+  getCompetitions,
+  deleteRound,
+} from "./controllers/shared/sharedEndpoints.js";
+import {
+  getRoundsPlayed,
+  postRound,
+} from "./controllers/players/roundEndPoint.js";
 import {
   changePlayerHandicap,
-} from "./controllers/players/playerEndpoints.js";
-import {
   registerCompetitionToPlayer,
   registerPlayerToCompetition,
-} from './controllers/players/playerEndpoints.js';
-import { login } from './controllers/login/login.js';
-
+} from "./controllers/players/playerEndpoints.js";
+import { login } from "./controllers/login/login.js";
 
 dotenv.config();
 
@@ -32,13 +33,13 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use('/api', routes);
-app.use(morgan('dev'));
+app.use("/api", routes);
+app.use(morgan("dev"));
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 const databaseConnection = async () => {
@@ -57,14 +58,16 @@ const databaseConnection = async () => {
 };
 databaseConnection();
 
-registerPlayerToCompetition();
-registerCompetitionToPlayer();
-//postPlayer();
-//deletePlayer();
-postCompetition();
-//getCompetitions();
-postRound();
-//setPatientInactive();
-//login();
-getRoundsPlayed();
+// registerPlayerToCompetition();
+// registerCompetitionToPlayer();
+// //postPlayer();
+// //deletePlayer();
+// postCompetition();
+// //getCompetitions();
+// postRound();
+// //setPatientInactive();
+// //login();
+// getRoundsPlayed();
+// deleteRound();
+
 export default app;
