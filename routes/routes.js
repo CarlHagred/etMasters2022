5f3e08b7ffe97db72859bb78126ab46f6b34d03c
JoelSven <62708808+JoelSven@users.noreply.github.com>
2022-05-04 10:31:32 +0200

    Admin Login

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,17 @@
-import express from "express";
+import express from 'express';
 
-import { postPlayer } from "../controllers/admin/adminEndpoints.js";
-import { getPlayers } from "../controllers/players/playerEndpoints.js";
-import { login } from "../controllers/login/login.js";
+import { postPlayer } from '../controllers/admin/adminEndpoints.js';
+import { getPlayers } from '../controllers/players/playerEndpoints.js';
+import { login, loginAdmin } from '../controllers/login/login.js';
 
 const router = express.Router();
 
-router.post("/newplayer/", postPlayer);
+router.post('/newplayer/', postPlayer);
 
-router.get("/getplayers/", getPlayers);
+router.get('/getplayers/', getPlayers);
 
-router.get("/loginplayer/", login);
+router.get('/loginplayer/', login);
+
+router.get('/loginadmin', loginAdmin);
 
 export default router;
