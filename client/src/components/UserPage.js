38cfafda93b4f05b0b7bd990d50227121dcedd3d
JoelSven <62708808+JoelSven@users.noreply.github.com>
2022-05-04 15:40:41 +0200

    runda

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getCompetitions, registerToCompetition } from "../api";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getCompetitions, registerRound, registerToCompetition } from '../api';
+import Card from './UI/Card';
 
 const UserPage = () => {
   const { id } = useParams();
   const [competitions, setCompetitions] = useState([]);
   const [selectedCompetition, setSelectedCompetiton] = useState({});
+
+  const [points, setPoints] = useState();
+  const [course, setCourse] = useState();
+  const [weather, setWeather] = useState();
+  const [mood, setMood] = useState();
 
   useEffect(() => {
     const fetchComp = async () => {
@@ -27,8 +33,25 @@ const UserPage = () => {
     registerToCompetition(params);
   };
 
+  const handlePostRound = (e) => {
+    e.preventDefault();
+
+    const params = {
+      points: points,
+      course: course,
+      weather: weather,
+      mood: mood,
+      playerId: id,
+      compId: selectedCompetition._id,
+    };
+
+    registerRound(params);
+  };
+
   return (
     <>
+      <h1>{`Du är inloggad som ${id}`}</h1>
+
       <form>
         <select
           multiple={false}
@@ -51,9 +74,82 @@ const UserPage = () => {
           Register to competition
         </button>
       </form>
-      <h1>{`Du är inloggad som ${id}`}</h1>
+      <Card>
+        <form>
+          <label>
+            Points:
+            <input
+              type="number"
+              name="points"
+              onChange={(e) => {
+                setPoints(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            {/* ska egentligen vara SELECT */}
+            Course:
+            <input
+              type="text"
+              name="course"
+              onChange={(e) => {
+                setCourse(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Weather:
+            <input
+              type="text"
+              name="weather"
+              onChange={(e) => {
+                setWeather(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Mood:
+            <input
+              type="text"
+              name="mood"
+              onChange={(e) => {
+                setMood(e.target.value);
+              }}
+            />
+          </label>
+          {/* Fusk programmerat, kolla på alternativ lösning, om tid finns */}
+          <select
+            multiple={false}
+            value={selectedCompetition.name}
+            onChange={(e) =>
+              setSelectedCompetiton(
+                competitions.find((element) => element.name === e.target.value)
+              )
+            }
+          >
+            {competitions.map((e, key) => {
+              return (
+                <option key={key} value={e.name}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            type="submit"
+            value="Register round"
+            onClick={handlePostRound}
+          />
+        </form>
+      </Card>
     </>
   );
 };
-
+/*
+ points: points,
+        weather: weather,
+        course: course,
+        mood: mood,
+        player: playerId,
+        */
 export default UserPage;
--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,28 @@
-import express from "express";
+import express from 'express';
 
-import { postPlayer } from "../controllers/admin/adminEndpoints.js";
+import { postPlayer } from '../controllers/admin/adminEndpoints.js';
 import {
   getPlayers,
   registerPlayerToCompetition,
-} from "../controllers/players/playerEndpoints.js";
-import { login, loginAdmin } from "../controllers/login/login.js";
-import { getCompetitions } from "../controllers/shared/sharedEndpoints.js";
+} from '../controllers/players/playerEndpoints.js';
+import { login, loginAdmin } from '../controllers/login/login.js';
+import { getCompetitions } from '../controllers/shared/sharedEndpoints.js';
+import { postRound } from '../controllers/players/roundEndPoint.js';
 
 const router = express.Router();
 
-router.post("/newplayer/", postPlayer);
+router.post('/newplayer/', postPlayer);
 
-router.get("/getplayers/", getPlayers);
+router.get('/getplayers/', getPlayers);
 
-router.get("/loginplayer/", login);
+router.get('/loginplayer/', login);
 
-router.get("/loginadmin", loginAdmin);
+router.get('/loginadmin', loginAdmin);
 
-router.get("/getcompetitions", getCompetitions);
+router.get('/getcompetitions', getCompetitions);
 
-router.post("/registertocompetition", registerPlayerToCompetition);
+router.post('/registertocompetition', registerPlayerToCompetition);
+
+router.post('/postround', postRound);
 
 export default router;
